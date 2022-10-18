7ec39263f8ae581da2b8b9fc5c360d012cf4b07a
Hulff <ph2717693@Gmail.com>
2022-10-18 20:28:53 -0300

    ooo

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
-const head = document.getElementById("1")
-const name = document.getElementById("2")
-const email = document.getElementById("3")
+const temperatura = document.getElementById("1")
+const pressao = document.getElementById("2")
+const bateria = document.getElementById("3")
+const payloadAltitude = document.getElementById("10")
+const payloadCo2 = document.getElementById("11")
+const payloadVoc = document.getElementById("12")
 const input = document.getElementById("input")
+let id 
 
+let dadosAtuais 
+let dadoAnterior 
 
-function search () {
-    console.log("inicio")
-fetch(`https://api-project-smoky.vercel.app/getInfo/`).then((response => response.json())).then( data =>{
+setTimeout(()=>{
+    search()
+},1000)
+
+function comparar() {
+
+}
+function getData() {
+    fetch(`https://api-project-smoky.vercel.app/getInfo/`).then((response => response.json())).then( data =>{
     console.log(data[0])
-    head.innerHTML= "temperatura = "+data[0].temperatura
-    name.innerHTML= "pressao = "+data[0].pressao
-    email.innerHTML= "bateria = "+data[0].bateria
-}).catch((err)=>{
 
-})
+    payloadAltitude.innerHTML = "altitude = " +data[0].payload.altitude;
+    payloadCo2.innerHTML = "CO2 = " +data[0].payload.co2;
+    payloadVoc.innerHTML = "VOC's = " +data[0].payload.voc;
+    temperatura.innerHTML= "temperatura = "+data[0].temperatura;
+    pressao.innerHTML= "pressao = "+data[0].pressao;
+    bateria.innerHTML= "bateria = "+data[0].bateria;
+    id = data[0]._id
+    dadosAtuais = {
+        temperatura:data[0].temperatura,
+        pressao:data[0].pressao,
+        voc:data[0].payload.voc,
+        co2:data[0].payload.co2,
+        altitude:data[0].payload.altitude,
+        bateria:data[0].bateria
+    }
+    }).catch((err)=>{
+    console.log(err)
+    })
+}
+function search () {
+    if (id == undefined) {
+        getData()
+} else {
+    dadoAnterior = dadosAtuais
+        getData()
+        comparar()
+    }
 }
