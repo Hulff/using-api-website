fae47e3885a12fc19471980a18d40e1418d71dcb
Hulff <ph2717693@Gmail.com>
2022-10-21 21:26:51 -0300

    igy8g

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-const temperatura = document.getElementById("1")
-const pressao = document.getElementById("2")
-const bateria = document.getElementById("3")
-const payloadAltitude = document.getElementById("10")
-const payloadCo2 = document.getElementById("11")
-const payloadVoc = document.getElementById("12")
-const input = document.getElementById("input")
-const temp = document.getElementById("4")
-const press = document.getElementById("5") 
-const battery = document.getElementById("6")
-const alt = document.getElementById("7")
-const voc = document.getElementById("8")
-const co2 = document.getElementById("9")
+const temperaturaHtml = document.getElementById("1")
+const pressaoHtml = document.getElementById("2")
+const bateriaHtml = document.getElementById("3")
+const payloadAltitudeHtml = document.getElementById("10")
+const payloadCo2Html = document.getElementById("11")
+const payloadVocHtml = document.getElementById("12")
+const inputHtml = document.getElementById("input")
+const tempHtml = document.getElementById("4")
+const pressHtml= document.getElementById("5") 
+const batteryHtml= document.getElementById("6")
+const altHtml = document.getElementById("7")
+const vocHtml = document.getElementById("8")
+const co2Html = document.getElementById("9")
 
 let id
 
 let dadosAtuais 
 let dadoAnterior 
 
-setTimeout(()=>{
-    search()
-},1000)
 
 function comparar() {
-    temp.innerHTML = `temperatura: ${(dadoAnterior.temperatura - dadosAtuais.temperatura)}`
-    battery.innerHTML = `bateria: ${(dadoAnterior.bateria - dadosAtuais.bateria)}`
-    press.innerHTML = `pressao: ${(dadoAnterior.pressao- dadosAtuais.pressao)}`
-    alt.innerHTML = `altitude: ${(dadoAnterior.altitude - dadosAtuais.altitude)}`
-    voc.innerHTML  = `VOC: ${(dadoAnterior.voc - dadosAtuais.voc)}`
-    co2.innerHTML = `co2: ${(dadoAnterior.co2 - dadosAtuais.co2)}`
+    tempHtml.innerHTML = `temperatura: ${(dadoAnterior.temperatura - dadosAtuais.temperatura)}`
+    batteryHtml.innerHTML = `bateria: ${(dadoAnterior.bateria - dadosAtuais.bateria)}`
+    pressHtml.innerHTML = `pressao: ${(dadoAnterior.pressao- dadosAtuais.pressao)}`
+    altHtml.innerHTML = `altitude: ${(dadoAnterior.altitude - dadosAtuais.altitude)}`
+    vocHtml.innerHTML  = `VOC: ${(dadoAnterior.voc - dadosAtuais.voc)}`
+    co2Html.innerHTML = `co2: ${(dadoAnterior.co2 - dadosAtuais.co2)}`
 }
 function getData() {
-    fetch(`https://api-project-smoky.vercel.app/getInfo/`).then((response => response.json())).then( data =>{
+    if (inputHtml.value == "") {
+            console.log("sem identificador")
+    } else {
+        let link = `https://api-project-smoky.vercel.app/getInfo/${inputHtml.value}`
+    fetch(link).then((response => response.json())).then( data =>{
     console.log(data[0])
 
-    payloadAltitude.innerHTML = "altitude = " +data[0].payload.altitude;
-    payloadCo2.innerHTML = "CO2 = " +data[0].payload.co2;
-    payloadVoc.innerHTML = "VOC's = " +data[0].payload.voc;
-    temperatura.innerHTML= "temperatura = "+data[0].temperatura;
-    pressao.innerHTML= "pressao = "+data[0].pressao;
-    bateria.innerHTML= "bateria = "+data[0].bateria;
+    payloadAltitudeHtml.innerHTML = "altitude = " +data[0].payload.altitude;
+    payloadCo2Html.innerHTML = "CO2 = " +data[0].payload.co2;
+    payloadVocHtml.innerHTML = "VOC's = " +data[0].payload.voc;
+    temperaturaHtml.innerHTML= "temperatura = "+data[0].temperatura;
+    pressaoHtml.innerHTML= "pressao = "+data[0].pressao;
+    bateriaHtml.innerHTML= "bateria = "+data[0].bateria;
     id = data[0]._id
     dadosAtuais = {
         temperatura:data[0].temperatura,
@@ -51,6 +52,7 @@ function getData() {
     }).catch((err)=>{
     console.log(err)
     })
+ }
 }
 function search () {
     if (id == undefined) {
@@ -60,6 +62,6 @@ function search () {
         getData()
         setTimeout(()=>{
             comparar()
-        },1500)
+        },1000)
     }
 }
