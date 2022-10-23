63af11f752878d6813771d4af8de7122876051df
Hulff <ph2717693@Gmail.com>
2022-10-23 17:20:01 -0300

    wsfjdk

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const ulRelatorioHtml = document.getElementById("ulRelatorio")
 const temperaturaHtml = document.getElementById("1")
 const pressaoHtml = document.getElementById("2")
 const bateriaHtml = document.getElementById("3")
@@ -11,6 +12,8 @@ const batteryHtml= document.getElementById("6")
 const altHtml = document.getElementById("7")
 const vocHtml = document.getElementById("8")
 const co2Html = document.getElementById("9")
+const timeout = setTimeout(search, 6000)
+clearTimeout(timeout)
 let id
 
 let dadosAtuais 
@@ -18,47 +21,72 @@ let dadoAnterior
 
 
 function comparar() {
-    let tempDif = dadosAtuais.temperatura - dadoAnterior.temperatura 
+        ulRelatorioHtml.style.backgroundColor = "#efeeee"
+        let tempDif = dadosAtuais.temperatura - dadoAnterior.temperatura 
+    
+        if(dadoAnterior.temperatura < dadosAtuais.temperatura) {
+            tempHtml.innerHTML = "temperatura: +"+ tempDif
+        } else {
+            tempHtml.innerHTML = "temperatura: "+ tempDif
+        }
+        let batteryDif = dadosAtuais.bateria - dadoAnterior.bateria
+        if(dadoAnterior.bateria < dadosAtuais.bateria) {
+            batteryHtml.innerHTML = "bateria: +"+ batteryDif
+        } else {
+            batteryHtml.innerHTML = "temperatura: "+ batteryDif
+        }
+    
+        let pressDif = dadosAtuais.pressao  - dadoAnterior.pressao 
+        if(dadoAnterior.pressao < dadosAtuais.pressao) {
+            pressHtml.innerHTML = "pressao: +"+pressDif
+        } else {
+            pressHtml.innerHTML = "pressao: "+pressDif
+        }
+        let altDif =  dadosAtuais.altitude-dadoAnterior.altitude
+        if(dadoAnterior.altitude < dadosAtuais.altitude) {
+            altHtml.innerHTML = "altitude: +"+altDif
+        } else {
+            altHtml.innerHTML = "altitude: "+altDif
+        }
+        let vocDif = dadosAtuais.voc-dadoAnterior.voc
+        if(dadoAnterior.altitude < dadosAtuais.altitude) {
+            vocHtml.innerHTML = "VOC: +"+vocDif
+        } else {
+            vocHtml.innerHTML = "VOC: "+vocDif
+        }
+        let co2Dif = dadosAtuais.co2-dadoAnterior.co2
+        if(dadoAnterior.co2 < dadosAtuais.co2) {
+            co2Html.innerHTML = "Co2: +"+co2Dif
+        } else {
+            co2Html.innerHTML = "Co2: "+co2Dif
+        }
+}
+function search() {
+    getData()
+    dadoAnterior = dadosAtuais
 
-    if(dadoAnterior.temperatura < dadosAtuais.temperatura) {
-        tempHtml.innerHTML = "temperatura: +"+ tempDif
-    } else {
-        tempHtml.innerHTML = "temperatura: "+ tempDif
+    if(keepGoing) {
+        setTimeout(search, 6000);
     }
-    batteryHtml.innerHTML = `bateria: -${(dadoAnterior.bateria - dadosAtuais.bateria)}`
+}
 
-    let pressDif = dadosAtuais.pressao  - dadoAnterior.pressao 
-    if(dadoAnterior.pressao < dadosAtuais.pressao) {
-        pressHtml.innerHTML = "pressao: +"+pressDif
-    } else {
-        pressHtml.innerHTML = "pressao: "+pressDif
-    }
-    let altDif =  dadosAtuais.altitude-dadoAnterior.altitude
-    if(dadoAnterior.altitude < dadosAtuais.altitude) {
-        altHtml.innerHTML = "altitude: +"+altDif
-    } else {
-        altHtml.innerHTML = "altitude: "+altDif
-    }
-    let vocDif = dadosAtuais.voc-dadoAnterior.voc
-    if(dadoAnterior.altitude < dadosAtuais.altitude) {
-        vocHtml.innerHTML = "VOC: +"+vocDif
-    } else {
-        vocHtml.innerHTML = "VOC: "+vocDif
-    }
-    let co2Dif = dadosAtuais.co2-dadoAnterior.co2
-    if(dadoAnterior.co2 < dadosAtuais.co2) {
-        co2Html.innerHTML = "Co2: +"+co2Dif
-    } else {
-        co2Html.innerHTML = "Co2: "+co2Dif
-    }
+function startLoop() {
+    keepGoing = true;
+    search();
+}
+
+function stopSearch() {
+    keepGoing = false;
+    clearTimeout(timeout)
 }
 function getData() {
     if (inputHtml.value == "") {
             console.log("sem identificador")
     } else {
         let link = (`https://api-project-smoky.vercel.app/getdata/${inputHtml.value}`).toString()
+
         fetch(link).then((response => response.json())).then( data =>{
-        console.log(data[0])
+        
 
     payloadAltitudeHtml.innerHTML = "altitude = " +data[0].payload.altitude;
     payloadCo2Html.innerHTML = "CO2 = " +data[0].payload.co2;
@@ -75,21 +103,20 @@ function getData() {
         altitude:data[0].payload.altitude,
         bateria:data[0].bateria
     }
+    console.log(dadosAtuais)
+    if(dadoAnterior != undefined) {
+        setTimeout(() => {
+            if(dadoAnterior.bateria == dadosAtuais.bateria) {
+                console.log("dados =")
+        } else {
+            console.log("dados diferentes")
+            comparar()
+        }
+        },500);
+    }
     }).catch((err)=>{
-    console.log(err)
     })
  }
 }
-function stopSearch() {
-    clearTimeout(timeout)
-}
-function search () {
-    if (id == undefined) {
-        getData()
-} else {
-    dadoAnterior = dadosAtuais
-        getData()  
-        setTimeout(comparar,1000)
-    }
-}
+      
       
