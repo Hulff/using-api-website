bcc291ae55e5fd26073898faf2cf2ffdd01ea07c
Hulff <ph2717693@Gmail.com>
2022-11-02 16:43:23 -0300

    sfdgfhgjh

--- homepage.js
+++ homepage.js
@@ -1,6 +1,5 @@
 const divHome = document.getElementById("div-home")
 const divAbout = document.getElementById("div-about")
-const divGuide = document.getElementById("div-guide")
 
 setTimeout( selectSection(1) , 500)
 
@@ -8,11 +7,9 @@ function selectSection(sectionId) {
     if (sectionId == 1) {
         divHome.style.display = "initial"
         divAbout.style.display = "none"
-        divGuide.style.display = "none"
     }
     if (sectionId == 2) {
         divAbout.style.display = "initial"
         divHome.style.display = "none"
-        divGuide.style.display = "none"
     }
 }
