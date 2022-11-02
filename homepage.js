9a91a235bad7f607eb5fcce2ff833b2fadcf15b2
Hulff <ph2717693@Gmail.com>
2022-11-01 23:21:08 -0300

    TGEHW79FH3

---
+++ homepage.js
@@ -0,0 +1,24 @@
+const divHome = document.getElementById("div-home")
+const divAbout = document.getElementById("div-about")
+const divGuide = document.getElementById("div-guide")
+
+
+setTimeout( selectSection(1) , 500)
+
+function selectSection(sectionId) {
+    if (sectionId == 1) {
+        divHome.style.display = "initial"
+        divAbout.style.display = "none"
+        divGuide.style.display = "none"
+    }
+    if (sectionId == 2) {
+        divAbout.style.display = "initial"
+        divHome.style.display = "none"
+        divGuide.style.display = "none"
+    }
+    if (sectionId == 3) {
+        divGuide.style.display = "initial"
+        divAbout.style.display = "none"
+        divHome.style.display = "none"
+    }
+}
